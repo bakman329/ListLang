06cbe983413351e80def0725340bc5b99949f658
Henry Sloan <bakmanthetitan329@gmail.com>
2016-05-07 14:45:29 -0400

    Change Options from a class to a namespace

--- Options.h
+++ Options.h
@@ -11,27 +11,24 @@
 
 #include "List.h"
 
-// TODO: Replace class with namespace, and change methods to non-static
-class Options {
-private:
+namespace Options {
     // Prints a help message with information about running the program
-    static void help_message();
+    void help_message();
 
     // Runs an interactive read–eval–print loop(REPL)
-    static void shell();
+    void shell();
 
     // Reads and runs instructions from a given file
     // If the file cannot be read, print an error
-    static void read_file(char *filename);
+    void read_file(char *filename);
 
     // Runs different actions based on the program arguments
     // If no defined operation is specified, show a help message
-    static void handle_options(char *option);
+    void handle_options(char *option);
 
-public:
     // If there are enough arguments to decide an action, pass the argument to handle_options
     // Else, give a help message
-    static void handle_args(int argc, char **argv);
+    void handle_args(int argc, char **argv);
 };
 
 #endif //LANGUAGE_OPTIONS_H
